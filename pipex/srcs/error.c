14a272ec8d924fe7fba5eaccc0e060171f5ce861
obvoso <autumninmoon@gmail.com>
2022-06-10 23:08:26 +0900

    Fix : pipex

---
+++ pipex/srcs/error.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/10 13:42:19 by soo               #+#    #+#             */
+/*   Updated: 2022/06/10 16:13:50 by soo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+int	check_arg(int ac, char **av)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	if (ac != 5)
+	{
+		ft_putstr_fd("Format Error : file1 \"cmd1\" \"cmd2\" file2\n", 2);
+		return (1);
+	}
+	if (check_slash_dot(av))
+	{
+		ft_putstr_fd("Format Error : file1 \"cmd1\" \"cmd2\" file2\n", 2);
+		return (1);
+	}
+	while (av[++i])
+	{
+		c = av[i][0];
+		if (!ft_isalnum(((int)c)) && c != '/' && c != '.')
+		{
+			ft_putstr_fd("Format Error : file1 \"cmd1\" \"cmd2\" file2\n", 2);
+			return (1);
+		}
+	}
+	return (0);
+}
+
+int	check_slash_dot(char **av)
+{
+	int		i;
+	char	c;
+
+	i = 0;
+	while (av[++i])
+	{
+		c = av[i][0];
+		if ((ft_strlen(av[i]) < 3 && c == '.'))
+			return (1);
+		if ((c == '/' && !ft_isalnum(av[i][1])) || ft_strlen(av[i]) == 1)
+			return (1);
+	}
+	return (0);
+}
--- pipex/srcs/pipex.c
+++ pipex/srcs/pipex.c
@@ -6,88 +6,11 @@
 /*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 20:51:51 by soo               #+#    #+#             */
-/*   Updated: 2022/06/09 22:58:52 by soo              ###   ########.fr       */
+/*   Updated: 2022/06/10 23:07:19 by soo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-char	**split_path(char **envp, char **argv, char ***cmd, int idx)
-{
-	char	**c_path;
-	int		i;
-
-	i = 0;
-	printf("%d\n", idx);
-	while (envp[i])
-	{
-		if (!ft_strncmp(envp[i], "PATH=", 5))
-		{
-			c_path = ft_split(ft_strdup(envp[i] + 5), ':');
-			if (!ft_strncmp(argv[idx], "awk", 3))
-			{
-				*cmd = ft_split(argv[idx], '\'');
-				cmd[0][0][3] = '\0';
-			}
-			else
-				*cmd = ft_split(argv[idx], ' ');
-			return (c_path);
-		}
-		++i;
-	}
-	return (NULL);
-}
-
-char	*check_path(char **path, char *cmd)
-{
-	int		i;
-	char	*tmp;
-	char	*c_path;
-
-	i = 0;
-	if (*cmd == '/')
-		if (!access(cmd, F_OK))
-			return (cmd);
-	while (path[i])
-	{
-		tmp = ft_strjoin(path[i], "/");
-		c_path = ft_strjoin(tmp, cmd);
-		free(tmp);
-		if (!access(c_path, F_OK))
-			return (c_path);
-		++i;
-		free(c_path);
-	}
-	ft_putstr_fd("command not found: ", 2);
-	ft_putstr_fd(cmd, 2);
-	ft_putstr_fd("\n", 2);
-	return (NULL);
-}
-
-void	second_cmd(char **argv, char **envp, int pipe[])
-{
-	int		fd;
-	char	*c_path;
-	char	**path;
-	char	**cmd;
-
-	path = split_path(envp, argv, &cmd, 3);
-	if (!path)
-		exit (1);
-	c_path = check_path(path, cmd[0]);
-	if (!c_path)
-		exit(1);
-	fd = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	if (fd == -1)
-		exit(1);
-	if ((dup2(fd, STDOUT_FILENO) == -1) || (dup2(pipe[0], STDIN_FILENO) == -1))
-		exit(1);
-	close(fd);
-	close(pipe[0]);
-	close(pipe[1]);
-	if (execve(c_path, cmd, envp) == -1)
-		exit(1);
-}
 
 void	first_cmd(char **argv, char **envp, int pipe[])
 {
@@ -103,7 +26,7 @@ void	first_cmd(char **argv, char **envp, int pipe[])
 		exit(1);
 	c_path = check_path(path, cmd[0]);
 	if (!c_path)
-		exit(1);
+		exit(127);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		exit(1);
@@ -116,40 +39,87 @@ void	first_cmd(char **argv, char **envp, int pipe[])
 		exit(1);
 }
 
+void	second_cmd(char **argv, char **envp, int pipe[])
+{
+	int		fd;
+	char	*c_path;
+	char	**path;
+	char	**cmd;
+
+	path = split_path(envp, argv, &cmd, 3);
+	if (!path)
+		exit(1);
+	fd = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if (fd == -1)
+		exit(1);
+	c_path = check_path(path, cmd[0]);
+	if (!c_path)
+		exit(127);
+	if ((dup2(fd, STDOUT_FILENO) == -1) || (dup2(pipe[0], STDIN_FILENO) == -1))
+		exit(1);
+	write(1, "1\n", 2);
+	close(fd);
+	close(pipe[0]);
+	close(pipe[1]);
+	if (execve(c_path, cmd, envp) == -1)
+		exit(1);
+}
+
+void	second_child(char **argv, char **envp, int fd[])
+{
+		pid_t	pid;
+
+		pid = fork();
+		if (pid == -1)
+			exit(1);
+		else if (pid == 0)
+		{
+			second_cmd(argv, envp, fd);
+		}
+		else
+			waitpid(pid, NULL, 0);
+}
+/*
+int	ddal_process(char **argv, char **envp, int fd[])
+{
+		int	pid;
+
+		pid = fork();
+		if (pid == -1)
+			return (1);
+		else if (pid == 0)
+			first_cmd(argv, envp, fd);
+		else if (pid > 0)
+		{
+			if (!ft_strncmp(argv[1], "/dev/urandom", 12))
+				waitpid(pid, NULL, WNOHANG);
+			else
+				waitpid(pid, NULL, 0);
+			return (second_cmd(argv, envp, fd));
+		}
+}
+*/
 int	main(int argc, char **argv, char **envp)
 {
 	pid_t	pid;
 	int		fd[2];
-	int		i;
 
-	i = 1;
-	if (argc != 5)
-	{
-		ft_putstr_fd("Format Error : file1 \"cmd1\" \"cmd2\" file2\n", 2);
+	if (check_arg(argc, argv))
 		return (1);
-	}
-	while (argv[i])
-	{
-		if (argv[i++][0] == '\0')
-		{
-			ft_putstr_fd("Format Error : file1 \"cmd1\" \"cmd2\" file2\n", 2);
-			return (1);
-		}
-	}
-	if (pipe(fd) == -1)
-		exit(1);
+	 if (pipe(fd) == -1)
+	 	return (1);
 	pid = fork();
 	if (pid == -1)
-		exit (1);
+		return  (1);
 	else if (pid == 0)
 		first_cmd(argv, envp, fd);
-	else if (pid > 0)
+	else
 	{
 		if (!ft_strncmp(argv[1], "/dev/urandom", 12))
 			waitpid(pid, NULL, WNOHANG);
 		else
 			waitpid(pid, NULL, 0);
-		second_cmd(argv, envp, fd);
+		second_child(argv, envp, fd);
 	}
 	return (0);
 }
