061a1d540bb7066155827edc8294e8ab02d688ce
obvoso <autumninmoon@gmail.com>
2022-06-12 22:24:01 +0900

    Feat : pipex

--- pipex/include/pipex_bonus.h
+++ pipex/include/pipex_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   pipex_bonus.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/05 20:50:38 by soo               #+#    #+#             */
-/*   Updated: 2022/06/12 20:58:52 by soo              ###   ########.fr       */
+/*   Created: 2022/06/12 21:02:21 by soo               #+#    #+#             */
+/*   Updated: 2022/06/12 21:11:07 by soo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
+#ifndef PIPEX_BONUS_H
+# define PIPEX_BONUS_H
 
 # include <fcntl.h>
 # include <stdlib.h>
@@ -21,13 +21,13 @@
 # include <unistd.h>
 # include <stdio.h>
 # include "../srcs/libft/libft.h"
+# include "pipex.h"
 
-//error
-int		check_arg(int argc, char **argv);
-int		check_slash_dot(char **av);
-
-//path
-char	**split_path(char **envp, char **argv, char ***cmd, int idx);
-char	*check_path(char **path, char *cmd);
+typedef struct s_pipex
+{
+	char	**cmd;
+	char	*c_path;
+	int		pipe[2];
+}t_pipex;
 
 #endif
