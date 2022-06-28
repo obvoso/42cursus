b2cc3b76e724d54edb57a2a83d8b2ee40c001e9c
Songmin O <soo@c3r2s4.42seoul.kr>
2022-06-28 16:58:23 +0900

    Feat : minishell

---
+++ minishell/env.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/28 14:02:34 by soo               #+#    #+#             */
+/*   Updated: 2022/06/28 16:55:48 by soo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "env.h"
+
+static t_env	*get_last(t_env *head)
+{
+	while (head->next)
+		head = head->next;
+	return (head);
+}
+
+void	str_free(char **str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		free(str[i++]);
+	free(str);
+}
+
+t_env	*split_env(t_env *head, char **envp)
+{
+	t_env	*new;
+	char	**split_equal;
+	int		i;
+
+	i = 0;
+	while (envp[i])
+	{
+		new = (t_env *)malloc(sizeof(t_env));
+		split_equal = ft_split(envp[i++], '=');
+		new->key = ft_strdup(split_equal[0]);
+		new->value = ft_strdup(split_equal[1]);
+		if (!head->key)
+		{
+			head->key = new->key;
+			head->value = new->value;
+			head->next = NULL;
+			free(new);
+		}
+		else
+			get_last(head)->next = new;
+		new->next = NULL;
+		str_free(split_equal);
+	}
+	return (head);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	t_env	head;
+
+	ft_memset(&head, 0, sizeof(t_env));
+	split_env(&head, envp);
+	
+	t_env *tmp = &head;
+	while (tmp)
+	{
+		printf("%s=%s\n", tmp->key, tmp->value);
+		tmp = tmp->next;
+	}
+}
---
+++ minishell/libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soo <soo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/13 15:05:25 by soo               #+#    #+#             */
+/*   Updated: 2022/03/21 15:41:39 by soo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ret;
+
+	ret = (void *)malloc(count * size);
+	if (!ret)
+		return (NULL);
+	ft_bzero(ret, count * size);
+	return (ret);
+}
