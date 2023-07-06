18abd207c5da9ad18bed0df909fee138f6ee03ac
obvoso <autumninmoon@gmail.com>
2023-07-06 22:14:13 +0900

    chore: wordpress dockerfile

--- inception/srcs/requirements/mariadb/tools/run.sh
+++ inception/srcs/requirements/mariadb/tools/run.sh
@@ -3,9 +3,8 @@
 if [ ! -e /var/lib/mysql/$DBNAME ]; then
     /usr/bin/mysql_install_db "--user=mysql" "--datadir=/var/lib/mysql"
     mysqld --user=mysql --console &
-    sleep 1
-    echo "stlqkf"
-    # mysql
+    #sleep 1
+
     mysql -e "create database $DBNAME;"
     mysql -e "create user '$DBADMIN'@'%' identified by '$DBADMINPW';"
     mysql -e "create user '$DBUSER'@'%' identified by '$DBUSERPW';"
@@ -13,8 +12,6 @@ if [ ! -e /var/lib/mysql/$DBNAME ]; then
     mysql -e "flush privileges;"
     mysql -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%';"
     mysql -e "flush privileges;"
-    # exit
-    mysql -e "show databases;"
 
     mysqladmin -u root shutdown
 fi
--- inception/srcs/requirements/wordpress/tools/run.sh
+++ inception/srcs/requirements/wordpress/tools/run.sh
@@ -1,1 +1,12 @@
 wp core download
+wp config create --dbhost=3306 --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBUSERPW
+wp config get
+wp core install --admin_user=$WP_ADMIN  \
+                --admin_pass=$WP_PASSWORD \
+                --admin_email=$WP_EMAIL \
+                --url=$DOMAIN_NAME \
+                --title='SOO WORLD'
+wp super-admin add $WP_ADMIN
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD
+
+php-fpm81 -F
