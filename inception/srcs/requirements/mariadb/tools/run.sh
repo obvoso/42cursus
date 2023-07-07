049971d0844a4258d4266dbe45c6eef97662575c
obvoso <autumninmoon@gmail.com>
2023-07-07 22:14:58 +0900

    feat: add wordpress in docker compose

--- inception/srcs/requirements/mariadb/tools/run.sh
+++ inception/srcs/requirements/mariadb/tools/run.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
-if [ ! -e /var/lib/mysql/$DBNAME ]; then
+if [ ! -e /var/lib/mysql/$DB_NAME ]; then
     /usr/bin/mysql_install_db "--user=mysql" "--datadir=/var/lib/mysql"
     mysqld --user=mysql --console &
-    #sleep 1
+    sleep 1
 
-    mysql -e "create database $DBNAME;"
-    mysql -e "create user '$DBADMIN'@'%' identified by '$DBADMINPW';"
-    mysql -e "create user '$DBUSER'@'%' identified by '$DBUSERPW';"
-    mysql -e "grant all privileges on $DBNAME.* to '$DBADMIN'@'%';"
+    mysql -e "create database $DB_NAME;"
+    mysql -e "create user '$DB_ADMIN'@'%' identified by '$DB_ADMINPW';"
+    mysql -e "create user '$DB_USER'@'%' identified by '$DB_USERPW';"
+    mysql -e "grant all privileges on $DB_NAME.* to '$DB_ADMIN'@'%';"
     mysql -e "flush privileges;"
-    mysql -e "grant all privileges on $DBNAME.* to '$DBUSER'@'%';"
+    mysql -e "grant all privileges on $DB_NAME.* to '$DB_USER'@'%';"
     mysql -e "flush privileges;"
 
     mysqladmin -u root shutdown
--- inception/srcs/requirements/wordpress/tools/run.sh
+++ inception/srcs/requirements/wordpress/tools/run.sh
@@ -1,10 +1,14 @@
 wp core download
-wp config create --dbhost=3306 --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBUSERPW
+
+# 동일한 네트워크에 있는 mariadb의 컨테이너네임:포트번호
+# 사용할 db config 파일 생성, mariadb 컨테이너에서 만든 db로 연결
+wp config create --dbhost=mariadb:3306 --dbname=$DB_NAME --dbuser=$DB_ADMIN --dbpass=$DB_ADMINPW
+# config파일 출력
 wp config get
-wp core install --admin_user=$WP_ADMIN  \
-                --admin_pass=$WP_PASSWORD \
+wp core install --url=$DOMAIN_NAME \
+                --admin_user=$WP_ADMIN \
+                --admin_password=$WP_PASSWORD \
                 --admin_email=$WP_EMAIL \
-                --url=$DOMAIN_NAME \
                 --title='SOO WORLD'
 wp super-admin add $WP_ADMIN
 wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD
